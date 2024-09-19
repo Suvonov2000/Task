e5865581c116385d1b32868103880fe19dbc3af8
Umidjon <suvonov200gmail.com>
2024-09-19 14:30:48 +0900

    first JS

---
+++ index.js
@@ -0,0 +1,11 @@
+const firstName = "Suvonov";
+const lastName = "Umidjon";
+const middleName = "Bakhtiyor ugli";
+
+console.log(firstName.concat(" " + lastName + " " + middleName));
+console.log(firstName + " " + lastName + " " + middleName);
+
+const n = 3;
+console.log(n ** 3);
+console.log(Math.pow(n, 2), Math.pow(n, 3));
+console.log(n * n, n * n * n);
