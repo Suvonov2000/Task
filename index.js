85a7222733e9597905635f5c1144c294ba3b4606
Suvonovumidjon <suvonov200@gmail.com>
2024-09-27 23:01:49 +0900

    task

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// let str = "Hello world";
+// let newStr = str.split("l").join("x");
+// console.log(newStr);
+
 let str = "Hello world";
-let newStr = str.split("l").join("x");
+let newStr = str.replaceAll("l", "x");
 console.log(newStr);
