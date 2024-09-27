1bb29339a23b68561a370a5708523ae12654654f
Suvonovumidjon <suvonov200@gmail.com>
2024-09-28 01:35:01 +0900

    Task

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-// let str = "Hello world";
-// let newStr = str.split("l").join("x");
-// console.log(newStr);
+const num = 15;
 
-// let str = "Hello world";
-// let newStr = str.replaceAll("l", "x");
-// console.log(newStr);
-
-// const arr = [1, 2, 3, 4];
-// let sum = 0;
-
-// for (let i = 0; i < arr.length; i++) {
-//   sum += arr[i];
-// }
-
-// console.log(sum);
-
-let day = 3;
-
-switch (day) {
-  case 1:
-    console.log("Sunday");
-    break;
-  case 2:
-    console.log("Monday");
-    break;
-  case 3:
-    console.log("Tuesday");
-    break;
-  case 4:
-    console.log("Wednesday");
-    break;
-  case 5:
-    console.log("Thurthday");
-    break;
-  case 6:
-    console.log("Friday");
-    break;
-  case 7:
-    console.log("Saturday");
-    break;
-  default:
+if (num / 3 && num / 5) {
+  console.log("Divisibility by both 3 or 5");
+} else if (num / 3) {
+  console.log("Divisibility by 3");
+} else if (num / 5) {
+  console.log("Divisibility by 5");
 }
