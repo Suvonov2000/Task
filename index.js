7b4ae386f0936410beafee0cb9212bd52eb78df4
Suvonovumidjon <suvonov200@gmail.com>
2024-09-28 00:47:41 +0900

    Task

--- index.js
+++ index.js
@@ -6,11 +6,38 @@
 // let newStr = str.replaceAll("l", "x");
 // console.log(newStr);
 
-const arr = [1, 2, 3, 4];
-let sum = 0;
+// const arr = [1, 2, 3, 4];
+// let sum = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
+// for (let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(sum);
+
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thurthday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  default:
 }
-
-console.log(sum);
