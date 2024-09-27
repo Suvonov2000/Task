c86300185e2717348cbf92235dacd0eafb8c451b
Suvonovumidjon <suvonov200@gmail.com>
2024-09-27 23:37:55 +0900

    Task

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 // let newStr = str.split("l").join("x");
 // console.log(newStr);
 
-let str = "Hello world";
-let newStr = str.replaceAll("l", "x");
-console.log(newStr);
+// let str = "Hello world";
+// let newStr = str.replaceAll("l", "x");
+// console.log(newStr);
+
+const arr = [1, 2, 3, 4];
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(sum);
