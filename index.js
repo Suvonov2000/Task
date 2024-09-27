561630e8335db8d6940b7554fa064deee2df84ef
Suvonovumidjon <suvonov200@gmail.com>
2024-09-28 03:36:40 +0900

    Hometask

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-let n = 100;
-let sum = 0;
-
-for (let i = 1; i <= n; i++) {
-  if (i % 2 === 0) {
-    sum += i;
-  }
-}
-console.log(sum);
+let str = "Hello world";
+let newStr = str.replaceAll("l", "x");
+console.log(newStr);
