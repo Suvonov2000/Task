0ce955c48306517c99cab8155d847f1a60b2cf0d
Suvonov2000 <suvonov200@gmail.com>
2024-10-07 09:09:40 +0900

    ex

---
+++ index.js
@@ -0,0 +1,6 @@
+const firstname = "Bakhtiyor";
+const lastname = "Mardonov";
+
+console.log(firstname + " " + lastname);
+console.log(`${firstname} ${lastname}`);
+console.log(firstname.concat(" ", lastname));
