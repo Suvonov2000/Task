3f0923495f5dc81b57105eeda2a0c8560d4418b9
Suvonov2000 <suvonov200@gmail.com>
2024-10-13 05:00:32 +0900

    func

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const isEven = function isEven(num) {
+  if (num % 2 === 0) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
 
-console.log(
-  arr.filter((value, index) => {
-    return value < 5;
-  })
-);
+console.log(isEven(8));
+console.log(isEven(7));
+
+let is_even = function is_even(number) {
+  return number % 2 === 0;
+};
+
+console.log(is_even(8));
+console.log(is_even(7));
