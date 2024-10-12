05ed8bbe6edb9d35b30bca5ede8fac72e3ef96e9
Suvonov2000 <suvonov200@gmail.com>
2024-10-12 19:36:32 +0900

    Masala

--- ex.js
+++
@@ -1,4 +0,0 @@
-var arr = [`Apple`, 12, `Banana`, `Orange`];
-
-console.log(arr.length);
-console.log(arr[0]);
--- index.js
+++
@@ -1,6 +0,0 @@
-const sum = 10;
-let n = 5;
-
-for (let i = 1; i <= sum; i++) {
-  console.log(n * i);
-}
